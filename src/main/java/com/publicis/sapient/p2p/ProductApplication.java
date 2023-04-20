11888cdb08bbe1f877762edf53a7c7e518535b81
Vishal Bharti <vishal.bharti@publicissapient.com>
2023-04-21 02:57:28 +0530

    v1 k8s-vishal-bharti-demo

---
+++ src/main/java/com/publicis/sapient/p2p/ProductApplication.java
@@ -0,0 +1,11 @@
+package com.publicis.sapient.p2p;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ProductApplication.class, args);
+	}
+}
---
+++ src/main/java/com/publicis/sapient/p2p/controller/DemoController.java
@@ -0,0 +1,14 @@
+package com.publicis.sapient.p2p.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+
+   @GetMapping(value = "/test")
+   public String checkApplication(){
+       return "Application Version *V1* is working Fine";
+    }
+}
